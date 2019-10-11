0bb31833b8456d0c13cb1fa1910d6412d7bbb266
Dai <shihengdaicoding@gmail.com>
2019-10-11 17:58:27 +1300

    2nd commit

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
+// vue.config.js
 module.exports = {
-<<<<<<< HEAD
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/EWGV2/'
-    : '/'
-}
-=======
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/EWGV2/'
-      : '/'
-  }
->>>>>>> f243d38c38c1fe710044dcbbef691a42138ff822
+  publicPath: ‘<EWGV2>’
+  } 
